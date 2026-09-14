d5d8eb52243081b6ef6b589cdeee4473961c1e7c
RRRC <rrivera2902@gmail.com>
2026-09-13 21:58:37 -0400

    Added board

---
+++ app/boards/my_board/board.cmake
@@ -0,0 +1,9 @@
+
+
+board_runner_args(jlink "--device=MCXA156")
+board_runner_args(linkserver "--device=MCXA156:FRDM-MCXA156")
+board_runner_args(pyocd "--target=mcxA156")
+
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
